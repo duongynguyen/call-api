7a4742d6fda37cadeb0f197b068549c33848b0dd
nguyenduong <duongynguyen0501@gmail.com>
2018-12-05 16:51:37 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="navbar navbar-default">
+          <a className="navbar-brand">CALL API</a>
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Products Managerment</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-3 col-sm-3 col-md-12 col-lg-12">
+
+              <button type="button" className="btn btn-info mb-10">Add Product</button>
+
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  <h3 className="panel-title">List of Product</h3>
+                </div>
+                <div className="panel-body">
+                  <table className="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>No.</th>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>iPhone</td>
+                        <td>500</td>
+                        <td>
+                          <span className="label label-warning">Still</span>
+                        </td>
+                        <td>
+                          <button type="button" className="btn btn-primary mr-10">Edit</button>
+                          <button type="button" className="btn btn-danger">Delete</button>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>Samsung</td>
+                        <td>600</td>
+                        <td>
+                          <span className="label label-warning">Sold out</span>
+                        </td>
+                        <td>
+                          <button type="button" className="btn btn-primary mr-10">Edit</button>
+                          <button type="button" className="btn btn-danger">Delete</button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
